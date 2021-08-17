053719d67485833ea74d34e6364e97e4ffefa785
Harsh Sachdeva <harsh.sachdeva@in.ibm.com>
2021-08-17 18:13:23 +0530

    initial commit

---
+++ src/main/java/com/test/app/OptyController.java
@@ -0,0 +1,51 @@
+package com.test.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.app.dto.EmpDetails;
+import com.test.app.sql.DBUtils;
+
+/**
+ * @author HarshSachdeva
+ *
+ */
+@RestController
+@RequestMapping("/springbootApp")
+public class OptyController {
+
+	Logger logger = LoggerFactory.getLogger(OptyController.class);
+
+	@Autowired
+	DBUtils dbUtils;
+	
+	@GetMapping("/empid/{id}")
+	public @ResponseBody EmpDetails getOptyArtifacts(@PathVariable(value = "id") String empid) {
+		logger.info("== Entering getOptyArtifacts : partnerid=  empid=" + empid);
+		EmpDetails empDetails = null;
+		if (empid!=null && !empid.equalsIgnoreCase("") && empid.equalsIgnoreCase("2468")) {
+		
+		// Get Opportunity details from database
+		 empDetails = dbUtils.getOptyDetails(empid);
+		
+		}
+		return empDetails;
+	}
+	
+	private boolean validateHeaderCredentials(String username, String password) {
+		logger.info("== Entering gvalidateHeaderCredentials== ");
+		boolean isAuthenticated = false;
+		if(username != null && password!= null && username.equals("harsh") && password.equals("india")) {
+			isAuthenticated = true;
+		}
+		logger.info("== Entering gvalidateHeaderCredentials== isAuthenticated=="+isAuthenticated);
+		return isAuthenticated;
+	}
+
+}
---
+++ src/main/java/com/test/app/sql/DBUtils.java
@@ -0,0 +1,25 @@
+package com.test.app.sql;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.test.app.OptyController;
+import com.test.app.dto.EmpDetails;
+
+@org.springframework.stereotype.Component
+public class DBUtils {
+
+	Logger logger = LoggerFactory.getLogger(OptyController.class);
+
+	public EmpDetails getOptyDetails(String empId) {
+		logger.info("==Entering getOptyDetails : get opportunity details from database for OptyId=" + empId);
+
+		EmpDetails empDetails = new EmpDetails();
+		empDetails.setEmpId(2468);
+		empDetails.setEmpName("Happy Harsh");
+		empDetails.setEmpLocation("Gurugram");
+		empDetails.setEmpDept("ABS");
+		
+		return empDetails;
+	}
+}
